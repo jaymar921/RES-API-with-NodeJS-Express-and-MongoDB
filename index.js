9020ae505c0405583c0e64383c6c3a1c99b7ef20
JayMar921 <jayharronabejar@gmail.com>
2023-08-08 20:57:14 +0800

    Updated Project Files - Added Dependency to Compression Library - Added Main route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const compression = require('compression')
 const app = express();
 
 const mongoose = require('mongoose');
@@ -9,16 +10,21 @@ console.log("Starting...")
 mongoose.connect(process.env.DATABASE_URL_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
+let port = process.env.PORT??80;
 
 db.on('error', (error) => console.log(error))
 db.once('open', ()=> console.log('Connected to Database'))
 
 app.use(express.json()); // lets the server accepts JSON
+app.use(compression()); // compress all routes
 
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
-app.listen(3000, () => {
-    console.log('Server Started');
+const mainRouter = require('./routes/main')
+app.use('/', mainRouter);
+
+app.listen(port, () => {
+    console.log(`Server Started, running on port: ${port}`);
 })
 
